49bfba2ef81888de1b0603f01c82191b8d8b3c49
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-27 17:04:35 -0300

    Aplicando Data Annotations

--- APICatalogo/Migrations/20250327200216_AjusteTabelas.Designer.cs
+++ APICatalogo/Migrations/20250327200216_AjusteTabelas.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using APICatalogo.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace APICatalogo.Migrations
 {
     [DbContext(typeof(ApiCatalogoContext))]
-    partial class ApiCatalogoContextModelSnapshot : ModelSnapshot
+    [Migration("20250327200216_AjusteTabelas")]
+    partial class AjusteTabelas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
